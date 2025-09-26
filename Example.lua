25e87057e7a07b62dca799f0a730553ac0c5b072
vitorffx <tff73436@gmail.com>
2025-09-25 23:16:42 -0300

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -1,5 +1,5 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/vitorffx/Sxz/refs/heads/main/SxzUi/SxzUi.luau"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Settings.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/vitorffx/Sxz/refs/heads/main/SxzUiSettings/SxzUiSettings.luau"))()
 
 local Window = Library:Start({
   ["Name"] = "Sxz Hub Ui | " .. Version,
